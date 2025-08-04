5cf1127e5beaf1b77d3119504da0dbf32816c053
Thilak Raj <admin@admins-MacBook-Pro.local>
2025-08-04 11:37:06 +0530

    stacks

---
+++ arrays/maxSubArray-kadanesalgo.js
@@ -0,0 +1,24 @@
+/**
+ * Kadane's algorithm
+ * Question: Given an integer array nums, find teh subarray with the largest sum, and return its sum.
+ * Input: [-2, 1, -3, 4, -1, 2, 1, -5, 4]         ==> Output: 6, [4, -1, 2, 1]
+ */
+
+function maxSubArray(nums){
+    let sum = 0;
+    let maxSum = nums[0];
+    for(let i=0; i<nums.length; i++){
+        sum+=nums[i];
+        if(sum>maxSum){
+            maxSum = sum;
+        }
+        if(sum<0){
+            sum = 0;
+        }
+      
+    }
+    return maxSum;
+}
+
+console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
+console.log(maxSubArray([5, 4, -1, 7, 8]))
